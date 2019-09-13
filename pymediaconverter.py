25ab60aa82284dd5a5a9b984080ef2b81303e61b
Jack Murray <jack@c0rporation.com>
2019-09-13 19:18:02 +0100

    We only care about CLOSE_WRITE events.

--- pymediaconverter.py
+++ pymediaconverter.py
@@ -5,7 +5,7 @@ wm = pyinotify.WatchManager()
 # Associate this WatchManager with a Notifier (will be used to report and
 # process events).
 notifier = pyinotify.Notifier(wm)
-# Add a new watch on /tmp for ALL_EVENTS.
-wm.add_watch('/tmp', pyinotify.ALL_EVENTS)
+# Add a new watch on /tmp for CLOSE_WRITE events (i.e. when a file is ready to be processed)
+wm.add_watch('/tmp', pyinotify.IN_CLOSE_WRITE)
 # Loop forever and handle events.
 notifier.loop()
